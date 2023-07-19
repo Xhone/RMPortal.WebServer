d3214a575c076475d6f896903d4ec8acb925f6d8
Xhone <659219179@qq.com>
2023-07-19 11:15:40 +0800

    u

--- Controllers/MpoController.cs
+++ Controllers/MpoController.cs
@@ -48,13 +48,17 @@ namespace RMPortal.WebServer.Controllers
 
 
         [HttpGet("GetMpoHd")]
-        public async Task<ActionResult<IEnumerable<MpoHd>>> GetMpoHd(string? mpo,DateTime? start,DateTime? end)
+        public async Task<ActionResult<IEnumerable<MpoHd>>> GetMpoHd(string? mpo,string? mpoType,string? supplier,string? status,string? jobNo,DateTime? start,DateTime? end)
         {
 
             var result = from hd in _context.TxMpoHds
                          where string.IsNullOrWhiteSpace(mpo) ? true : hd.MpoNo.Contains(mpo)
                          where start.HasValue ? hd.MpoDate >= start : true
                          where end.HasValue ? hd.MpoDate <= end : true
+                         where string.IsNullOrWhiteSpace(mpoType)?true:hd.MpoType.Contains(mpoType)
+                         where string.IsNullOrWhiteSpace(supplier)?true:hd.SuppCode.Contains(supplier)
+                         where string.IsNullOrWhiteSpace(jobNo)?true:hd.JobNoStr.Contains(jobNo)
+                         where string.IsNullOrWhiteSpace(status) ? true : hd.Status.Equals(status[0])
                          orderby hd.MpoDate descending, hd.Heading
                          select new MpoHd
                          {
@@ -244,12 +248,50 @@ namespace RMPortal.WebServer.Controllers
           {
               return Problem("Entity set 'RMPortalContext.TxMpoHds'  is null.");
           }
+
+            if (txMpoHd.TxMpoDets.Count > 0 && txMpoHd.TxMpoMatDets.Count == 0)
+            {
+               
+                txMpoHd.TxMpoMatDets = (from s in txMpoHd.TxMpoDets
+                                    group s by new
+                                    {
+                                        s.MatCode,
+                                        s.TempMat,
+                                        s.BuyUnit,
+                                        s.BuyUnitFactor,
+                                        s.PxUnit,
+                                        s.PxUnitFactor,
+                                        s.Width,
+                                        s.Weight,
+                                        s.MatDesc
+                                    } into g
+                                    select new TxMpoMatDet
+                                    {
+                                        MpoMatDetId = 0,
+                                        MpoNo = txMpoHd.MpoNo,
+                                        MatCode = g.Key.MatCode,
+                                        TempMat = g.Key.TempMat,
+                                        MpoAmount = g.Sum(s => s.Upx / (s.PxUnitFactor ?? 1) * s.Qty),
+                                        BuyUnit = g.Key.BuyUnit,
+                                        BuyUnitFactor = g.Key.BuyUnitFactor,
+                                        PriceUnit = g.Key.PxUnit,
+                                        PriceUnitFactor = g.Key.PxUnitFactor,
+                                        Width = g.Key.Width,
+                                        Weight = g.Key.Weight,
+                                        MatDesc = g.Key.MatDesc,
+                                        Remark = "",
+                                        Origin = "",
+
+
+                                        TxMpoHd = null,
+                                    }).ToList();
+            }
             _context.TxMpoHds.Add(txMpoHd);
             try
             {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException ex)
+            catch (SqlException ex)
             {
 
             }
